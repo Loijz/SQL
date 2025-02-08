1938bbbd1b4aeaef891afc1e66c180ca25b05652
Jarkko Schaad <149576263+Loijz@users.noreply.github.com>
2025-02-08 12:34:31 +0800

    Create select statement tutorial.sql
    first entry SQL code

---
+++ select statement tutorial.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+# PEMDAS
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
+
+SELECT 
+employee_id, 
+first_name, 
+last_name
+FROM parks_and_recreation.employee_demographics;
